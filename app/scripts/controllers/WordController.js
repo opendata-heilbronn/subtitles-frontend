ffb9335717126960be5d677ba4043dd2ecd5cab0
chiara <chiara.schepke@gmail.com>
2015-10-17 18:03:14 +0200

    finalchanges

--- app/scripts/controllers/WordController.js
+++ app/scripts/controllers/WordController.js
@@ -1,8 +1,25 @@
 (function () {
     'use strict';
 
-    angular.module('app').controller('WordController', function($scope, $routeParams) {
-        var word = {'english': 'apple', 'german': ['Apfel','Apple-company']};
-        $scope.word = word;
+    var words = {
+        "write": ["schreiben"],
+        "coder": ["Programmierer", "Softwareentwickler"],
+        "apple": ["Apfel", "Apple Company"],
+        "banana": ["Bananen"]
+    }
+    angular.module('app').controller('WordController', function ($scope, $routeParams, $location) {
+        var wordId=0; 
+        var getWord=function() {
+        var englishWord=Object.keys(words)[wordId];    
+            return {'english': englishWord, 'german': words[englishWord]}
+        }
+        $scope.word = getWord();
+       
+        $scope.next = function () {
+            wordId++;            
+            if (wordId >= Object.keys(words).length) wordId = 0;
+            $scope.word = getWord();
+            $scope.showTranslation = false;
+        }
     })
 })();
